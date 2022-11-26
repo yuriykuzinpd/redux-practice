c625b3996c4430d1fb8a533dcf31e36478e7d888
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-26 10:51:36 +0200

    base functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-import './styles.css'
+import "./styles.css";
+
+const counter = document.getElementById("counter");
+const addButton = document.getElementById("add");
+const subButton = document.getElementById("sub");
+const asyncButton = document.getElementById("async");
+const themeButton = document.getElementById("theme");
+
+let state = 0;
+
+addButton.addEventListener("click", () => {
+  state++;
+  render();
+});
+subButton.addEventListener("click", () => {
+  state--;
+  render();
+});
+asyncButton.addEventListener("click", () => {
+  setTimeout(() => {
+    state = 0;
+    render();
+  }, 2000);
+  render();
+});
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+function render() {
+  counter.textContent = state.toString();
+  render();
+}
+
+render();
