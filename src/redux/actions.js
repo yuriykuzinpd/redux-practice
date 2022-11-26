90d4882af7fa1b9fa203acd096f60c970a20ea77
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-26 18:43:15 +0200

    added action creators

---
+++ src/redux/actions.js
@@ -0,0 +1,12 @@
+import { DECREMENT, INCREMENT } from "./types";
+
+export function increment() {
+  return {
+    type: INCREMENT,
+  };
+}
+export function decrement() {
+  return {
+    type: DECREMENT,
+  };
+}
