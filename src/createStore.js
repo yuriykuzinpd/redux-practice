dac28010853755b6c206fb318e41b6da3faa16f9
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-26 18:08:08 +0200

    updated createStore.js

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,20 @@
-export function createStore() {
-    return {
-        dispatch(){},
-        subscribe(){},
-        getState(){},
+export function createStore(rootReducer, initialState) {
+
+  let state = rootReducer(initialState, {type: '__INIT__'});
+  const subscribers = [];
+
+  return {
+    dispatch(action) {
+        state = rootReducer(state, action);
+        subscribers.forEach(sub => sub());
+    },
+
+    subscribe(callback) {
+        subscribers.push(callback);
+    },
+
+    getState() {
+      return state;
     }
+  };
 }
