c3b4008ccc03d471bc9e8a9c9873b9ea86d15001
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-26 18:15:14 +0200

    added reducer

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,12 @@
+export function rootReducer(state, action) {
+
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  }
+
+  if (action.type === "DECREMENT") {
+    return state - 1;
+  }
+  
+  return state;
+}
